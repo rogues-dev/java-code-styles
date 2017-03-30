206a040b8e188f6ef4f04c4bc64482be3ab7ff6a
Andrew Reitz <aj.reitz@gmail.com>
2017-03-30 08:43:14 -0500

    Updated to Rogue's Dev
    Keep the styles the same, but updated wording and config names

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
-REM Installs Square's IntelliJ configs into your user configs.
+REM Installs Rogue's Dev IntelliJ configs into your user configs.
 @echo off
-echo Installing Square IntelliJ configs...
+echo Installing Rogue's Dev IntelliJ configs...
 
 setlocal enableDelayedExpansion
 
@@ -9,7 +9,7 @@ for /D %%i in ("%userprofile%"\.IdeaIC*) do call :copy_config "%%i"
 for /D %%i in ("%userprofile%"\.IntelliJIdea*) do call :copy_config "%%i"
 
 echo.
-echo Restart IntelliJ and/or AndroidStudio, go to preferences, and apply 'Square' or 'SquareAndroid'.
+echo Restart IntelliJ and/or AndroidStudio, go to preferences, and apply 'RoguesDev' or 'RoguesDevAndroid'.
 exit /b
 
 REM sub function for copy config files
